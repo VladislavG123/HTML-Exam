18d7913bf26465cfc4bf4b416fb1ac5473dade7f
vladislavg123 <gvo_step2018@mail.ru>
2022-02-20 23:47:02 +0600

    Assignment 4

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,5 @@
-$(document).ready = () => {
-    let products = JSON.parse(localStorage.getItem("f-products"))
-
-    for (const product of products) {
-        let imageId = Math.floor(Math.random() * 4) + 1;
-        $("#f-products").append(`
-                <div class="featured-product">
-                    <img src="img/pr-2-${imageId}.png" alt="${imageId}">
-                    <div class="product-info">
-                        <h3>${product.title}</h3>
-                        <p class="desc-text">${product.details}</p>
-                        <div class="sell-box">
-                            <i class="fas fa-exchange-alt"></i>
-                            <i class="fas fa-gift"></i>
-                            <i class="fas fa-shopping-cart add-product-to-card-button" product_id=\"${product.id}\"></i>
-                            <p>\$${product.price}.<sup>00</sup></p>
-                        </div>
-                    </div>
-                </div>`)
-    }
-
+window.setTimeout(() => {
+    console.log(123)
     $(".add-product-to-card-button").each((index, item) => {
         item.onclick = () => {
             let cardContent = JSON.parse(localStorage.getItem("cart"))
@@ -27,25 +8,32 @@ $(document).ready = () => {
             }
 
             let id = item.attributes.getNamedItem("product_id").nodeValue
-            cardContent.push()
+            cardContent.push(id)
 
-            localStorage.setItem("card", JSON.stringify(cardContent))
+            localStorage.setItem("cart", JSON.stringify(cardContent))
 
             new Audio("mixkit-alert-quick-chime-766.wav").play().then(r => alert(`Product #${id} is added to card`));
         }
     })
+}, 1000)
 
+$(document).ready(() => {
     products = JSON.parse(localStorage.getItem("f-products"))
     let cartContent = JSON.parse(localStorage.getItem("cart"))
 
     let productsInCart = []
     for (const product of products) {
-        if (cartContent.contains(product.id)) {
-            productsInCart.push(product)
+        if (cartContent.includes(product.id.toString())) {
+            $("#products-in-cart-list").append(`
+            <tr>
+                <th>${product.id}</th>
+                <td>${product.title}</td>
+                <td>\$${product.price}.00</td>
+            </tr>`)
         }
     }
 
     console.log(productsInCart)
-}
+})
 
-//$("#products-in-cart-list").append()
+//
